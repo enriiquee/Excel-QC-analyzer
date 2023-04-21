ae0f0b6914a2968d4ffa217d8ec69d654114c9e7
enriiqueef <enrique.perez@ucm.es>
2023-04-21 16:01:12 +0200

    v1 QC script

--- Script_QC.py
+++ Script_QC.py
@@ -1,15 +1,57 @@
-import tkinter as tk
-from tkinter import filedialog
-from tkinter.ttk import Progressbar
-import threading
-import pandas as pd
-import openpyxl
-from openpyxl.styles import PatternFill
-from openpyxl.utils import get_column_letter
+# Import the necessary libraries
+import subprocess
 
+# Check if pandas is installed
+try:
+    import pandas as pd
+except ImportError:
+    # Install pandas using pip if not found
+    subprocess.check_call(["python", "-m", "pip", "install", "pandas"])
+    import pandas as pd
+
+# Check if openpyxl is installed
+try:
+    import openpyxl
+    # Import required modules from openpyxl
+    from openpyxl.styles import Color, PatternFill, Font, Border
+    from openpyxl.utils import get_column_letter
+    from openpyxl.formatting.rule import ColorScaleRule, CellIsRule, FormulaRule
+
+except ImportError:
+    # Install openpyxl using pip if not found
+    subprocess.check_call(["python", "-m", "pip", "install", "openpyxl"])
+    import openpyxl
+    # Import required modules from openpyxl
+    from openpyxl.styles import Color, PatternFill, Font, Border
+    from openpyxl.utils import get_column_letter
+    from openpyxl.formatting.rule import ColorScaleRule, CellIsRule, FormulaRule
+
+# Check if tkinter is installed
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+    from tkinter.ttk import Progressbar
+except ImportError:
+    # Install tkinter using pip if not found
+    subprocess.check_call(["python", "-m", "pip", "install", "tkinter"])
+    import tkinter as tk
+    from tkinter import filedialog
+    from tkinter.ttk import Progressbar
+
+# Check if threading is installed
+try:
+    import threading
+except ImportError:
+    # Install threading using pip if not found
+    subprocess.check_call(["python", "-m", "pip", "install", "threading"])
+    import threading
+
+
+# Custom exception to be raised if the format of the Excel file is invalid
 class InvalidFormatException(Exception):
     pass
 
+# Function to check if the format of the Excel file is correct
 def is_format_correct(df):
     columns_to_check = ["Scan name", "ROI name", "Segment name", "Tags", "QC status",
                         "Binding Density", "FoV registration QC", "Positive norm factor",
@@ -23,6 +65,7 @@ def process_excel():
     thread = threading.Thread(target=process_excel_thread)
     thread.start()
 
+# Function to process the Excel file in a separate thread
 def process_excel_thread():
     global file_path
     if not file_path:
@@ -30,11 +73,11 @@ def process_excel_thread():
         return
 
     try:
-        # Etapa 1: Leer el archivo de Excel
+        # Step 1: Read the Excel file
         update_progress(10)
         df = pd.read_excel(file_path)
 
-        # Comprueba si el archivo tiene el formato correcto
+        # Check if the Excel file has the correct format
         if not is_format_correct(df):
             raise InvalidFormatException("El archivo no contiene el formato esperado.")
 
@@ -46,7 +89,7 @@ def process_excel_thread():
 
         df_qc_warning = df_qc[df_qc["QC status"].str.contains("WARNING")]
 
-        with pd.ExcelWriter("new_filename.xlsx", engine="openpyxl") as writer:
+        with pd.ExcelWriter("QC_filter_test.xlsx", engine="openpyxl") as writer:
             df_qc.to_excel(writer, sheet_name="QC", index=False)
             df_qc_warning.to_excel(writer, sheet_name="QC_Warning", index=False)
 
@@ -95,20 +138,23 @@ def process_excel_thread():
         output_text.set(f"Error: {e}")
     except Exception as e:
         output_text.set(f"Error inesperado: {e}")
-
+# Function to open the file dialog and select an Excel file
 def open_file_dialog():
     global file_path
-    file_path = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx")])
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
     file_path_text.set(file_path)
 
+# Function to update the progress bar
 def update_progress(percentage):
     progress_bar["value"] = percentage
     root.update_idletasks()
 
+# Function to close the app after a delay of 5 seconds
 def close_app_after_delay():
     global root
     root.destroy()
 
+# Function to run the app
 def run_app():
     global root, file_path, file_path_text, output_text, progress_bar
 
